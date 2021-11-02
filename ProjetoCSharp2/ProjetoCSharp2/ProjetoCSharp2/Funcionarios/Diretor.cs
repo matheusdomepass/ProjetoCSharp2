d69c0f81dc3bac4cfd3e3aa13a82185cf2f08250
Matheus Passatuto <50487734+matheusdomepass@users.noreply.github.com>
2021-11-02 16:01:05 -0300

    Add files via upload

---
+++ ProjetoCSharp2/ProjetoCSharp2/ProjetoCSharp2/Funcionarios/Diretor.cs
@@ -0,0 +1,28 @@
+﻿using BancoDigital.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDigital.Funcionarios
+{
+    public class Diretor : Funcionario
+    {
+        public Diretor(string cpf) : base(5000, cpf)
+        {
+            Console.WriteLine("Criando DIRETOR");
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.15;
+        }
+
+        public override double GetBonificacao()
+            {
+                return Salario * 0.5;
+            }
+        }
+    }
+
